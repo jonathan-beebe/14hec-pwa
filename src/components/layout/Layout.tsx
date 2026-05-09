e1f1a8ff62175cda6913aba35900d7f4cb42eb6b
Jonathan Beebe <jonathan-beebe@outlook.com>
2026-05-09 18:35:05 -0500

    fix(layout): allow main column to shrink so wide content scrolls instead of squishing sidebar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout() {
       <div className="noise-overlay" />
 
       <Sidebar />
-      <main className="flex-1 flex flex-col relative z-[1] min-h-0">
+      <main className="flex-1 flex flex-col relative z-[1] min-h-0 min-w-0">
         <div className="page-enter flex-1 overflow-y-auto min-h-0">
           <Outlet />
         </div>
